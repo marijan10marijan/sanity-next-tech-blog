c966d19e03075cd0c125501f828c586f28d7a80f
marijan10marijan <mmarijan39@gmail.com>
2024-11-24 20:57:20 +0100

    changes added

--- sanity/sanity.cli.js
+++ sanity/sanity.cli.js
@@ -1,9 +1,12 @@
 import {defineCliConfig} from 'sanity/cli'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default defineCliConfig({
   api: {
     projectId: process.env.SANITY_PROJECT_ID,
-    dataset: 'production',
+    dataset: process.env.SANITY_DATASET,
   },
   /**
    * Enable auto-updates for studios.
--- sanity/sanity.config.js
+++ sanity/sanity.config.js
@@ -2,13 +2,16 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default defineConfig({
   name: 'default',
   title: 'nextjs-14-blog',
 
-  projectId: 'qk7cy3tg',
-  dataset: 'production',
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
 
   plugins: [structureTool(), visionTool()],
 
