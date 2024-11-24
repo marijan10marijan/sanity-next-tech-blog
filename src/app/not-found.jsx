ec5ba56c259daf253e9d91f2f2aeb60c7d7dbadc
marijan10marijan <mmarijan39@gmail.com>
2024-11-24 20:39:24 +0100

    changes added

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -6,7 +6,7 @@ export default function NotFound() {
     <div className="flex flex-col  justify-center h-[calc(100vh-340px)]">
       <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
       <p className="mt-4 text-lg text-gray-500">
-        Oops! The page you're looking for doesn't exist.
+        Oops! The page you&apos;re looking for doesn&apos;t exist.
       </p>
       <Link
         className="mt-6 px-4 py-2 bg-primary text-white rounded-md hover:bg-gradient-to-tr hover:from-indigo-700 hover:to-purple-800 w-fit transition duration-200"
