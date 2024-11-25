e2f8adbbaf61cb459b4b5f39f16cdde040e7c2c0
marijan10marijan <mmarijan39@gmail.com>
2024-11-25 10:47:06 +0100

    changes

--- src/components/blog-card.jsx
+++ src/components/blog-card.jsx
@@ -9,7 +9,7 @@ const BlogCard = ({ blog, isPopularBlog }) => {
       {isPopularBlog ? (
         <div
           href={`/blog/${blog.slug}`}
-          className="flex-col gap-4 pt-0 px-2  sm:items-start"
+          className="flex-col gap-4 pt-0  sm:items-start"
         >
           <div
             className={`${isPopularBlog ? "h-28 sm:h-40 relative overflow-hidden " : "flex-1 sm:flex-auto w-full h-full sm:h-40 relative overflow-hidden"}`}
@@ -23,20 +23,13 @@ const BlogCard = ({ blog, isPopularBlog }) => {
               priority={true}
             />
           </div>
-          <div
-            className={`${isPopularBlog ? "pt-4 pb-0 px-2  flex flex-col gap-4 sm:flex-auto md:min-h-[180px]" : "py-4 px-2  flex flex-col gap-4 flex-1 sm:flex-auto justify-between md:min-h-[200px]"}`}
-          >
+          <div className="pt-4 pb-0 px-2  flex flex-col gap-4 sm:flex-auto md:min-h-[180px]">
             <h2 className="text-xl font-bold font-serif leading-6 line-clamp-2">
               {blog.title}
             </h2>
             <p className="font-sans text-sm sm:text-balance text-slate-800 dark:text-slate-300 line-clamp-3">
               {blog.desc}
             </p>
-            {!isPopularBlog && (
-              <button className="underline font-semibold text-start sm:hidden">
-                Read more
-              </button>
-            )}
             <div className="mt-auto">
               <p className="text-xs font-semibold text-gray-800 dark:text-gray-200">
                 {new Intl.DateTimeFormat("en-US", {
