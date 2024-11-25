257f0487832f6508f91e769939dc2f2542161734
marijan10marijan <mmarijan39@gmail.com>
2024-11-25 10:48:28 +0100

    changes

--- src/components/blog-card.jsx
+++ src/components/blog-card.jsx
@@ -56,20 +56,18 @@ const BlogCard = ({ blog, isPopularBlog }) => {
               priority={true}
             />
           </div>
-          <div
-            className={`${isPopularBlog ? "pt-4 pb-0 px-2  flex flex-col gap-4 sm:flex-auto md:min-h-[180px]" : "py-4 px-2  flex flex-col gap-4 flex-1 sm:flex-auto justify-between md:min-h-[200px]"}`}
-          >
+          <div className="py-4 px-2  flex flex-col gap-4 flex-1 sm:flex-auto justify-between md:min-h-[200px]">
             <h2 className="text-xl font-bold font-serif leading-6 line-clamp-2">
               {blog.title}
             </h2>
             <p className="font-sans text-sm sm:text-balance text-slate-800 dark:text-slate-300 line-clamp-3">
               {blog.desc}
             </p>
-            {!isPopularBlog && (
-              <button className="underline font-semibold text-start sm:hidden">
-                Read more
-              </button>
-            )}
+
+            <button className="underline font-semibold text-start sm:hidden">
+              Read more
+            </button>
+
             <div className="mt-auto">
               <p className="text-xs font-semibold text-gray-800 dark:text-gray-200">
                 {new Intl.DateTimeFormat("en-US", {
